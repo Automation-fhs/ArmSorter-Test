deb3e4211d5167d4b49ef4196cfa33696d1e3c90
Automation-fhs <fahasa.robot@gmail.com>
2024-06-12 10:19:31 +0700

    Modify private properties

--- Arm.js
+++ Arm.js
@@ -7,53 +7,53 @@ let availableMotorVoltage = [5,9,12,24,36,48,96,110,220];
 let limitDistance = [0, 20];
 
 class Arm {
-    #port;
-    #port_Distance;
-    #deli_ptnr;
-    #motor_V;
-    #motor_Enc_Res;
+    port;
+    port_Distance;
+    deli_ptnr;
+    motor_V;
+    motor_Enc_Res;
     constructor(position, delivery_partner){
-        this.#port = position;
-        this.#deli_ptnr = delivery_partner;
-        this.#motor_V = 24;
-        this.#motor_Enc_Res = 1000;
+        this.port = position;
+        this.deli_ptnr = delivery_partner;
+        this.motor_V = 24;
+        this.motor_Enc_Res = 1000;
     }
     get curPosition() {
-        return this.#port;
+        return this.port;
     }
     get curDeliveryPartner() {
-        return this.#deli_ptnr;
+        return this.deli_ptnr;
     }
     get curVoltage() {
-        return this.#motor_V;
+        return this.motor_V;
     }
     get curEncoderResolution() {
-        return this.#motor_Enc_Res;
+        return this.motor_Enc_Res;
     }
     get distFromSensor() {
-        return this.#port_Distance;
+        return this.port_Distance;
     }
     set newDeliveryPartner(deliveryPartner) {
         if(deliPartner.includes(deliveryPartner))
-            this.#deli_ptnr = deliveryPartner;
+            this.deli_ptnr = deliveryPartner;
         else 
             alert(`${deliveryPartner} is not a Delivery Partner!`);
     }
     set newMotorVoltage(voltage) {
-        if(availableMotorVoltage.includes(voltage)) this.#motor_V = voltage;
+        if(availableMotorVoltage.includes(voltage)) this.motor_V = voltage;
         else alert(`${voltage} is not a motor voltage!`);
     }
     set moveArm(distance) {
-        if(distance >= limitDistance[0] && distance <= limitDistance[1]) this.#port_Distance = distance;
+        if(distance >= limitDistance[0] && distance <= limitDistance[1]) this.port_Distance = distance;
         else alert(`Please adjust distance between ${limitDistance[0]} and ${limitDistance[1]}`);
     }
     armOpen() {
-        console.log(`Arm opening at port ${this.#port}`);
+        console.log(`Arm opening at port ${this.port}`);
         setTimeout(() => {this.armClose();},1500);
     }
 
     armClose() {
-        console.log(`Arm closing at port ${this.#port}`);
+        console.log(`Arm closing at port ${this.port}`);
     }
     
 }
